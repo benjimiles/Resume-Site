169604858d684a07f408dd2ece52887f7832fdc6
Benji Miles <admin@codingwithbenji.com>
2023-10-11 16:05:50 +0200

    commented out the animation for image on index

--- apps/portfolio-site/src/contents/index/Header/HeaderImage.tsx
+++ apps/portfolio-site/src/contents/index/Header/HeaderImage.tsx
@@ -1,29 +1,29 @@
 import clsx from 'clsx';
-import { m, useAnimationControls } from 'framer-motion';
+// import { m, useAnimationControls } from 'framer-motion';
 import Image from 'next/image';
 
-import HeaderImageAnimation from './HeaderImageAnimation';
+// import HeaderImageAnimation from './HeaderImageAnimation';
 
 function HeaderImage() {
-  const controlsHeaderImage = useAnimationControls();
-  const controlsHeaderOutline = useAnimationControls();
+  // const controlsHeaderImage = useAnimationControls();
+  // const controlsHeaderOutline = useAnimationControls();
 
   return (
     <div
-      className={clsx('relative h-[590px] w-[603px]')}
+      className={clsx('relative h-[590px] w-[650px]')}
       style={{
-        maskImage: `url("data:image/svg+xml,%3Csvg width='603' height='590' fill='none' viewBox='0 0 603 590' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m0 0v393h228v9.5c0 103.55 83.947 187.5 187.5 187.5s187.5-83.947 187.5-187.5v-402.5h-603z' fill='%23000'/%3E%3C/svg%3E%0A")`,
-        WebkitMaskImage: `url("data:image/svg+xml,%3Csvg width='603' height='590' fill='none' viewBox='0 0 603 590' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m0 0v393h228v9.5c0 103.55 83.947 187.5 187.5 187.5s187.5-83.947 187.5-187.5v-402.5h-603z' fill='%23000'/%3E%3C/svg%3E%0A")`,
+        maskImage: `url("data:image/svg+xml,%3Csvg width='650' height='590' fill='none' viewBox='0 0 603 590' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m0 0v393h228v9.5c0 103.55 83.947 187.5 187.5 187.5s187.5-83.947 187.5-187.5v-402.5h-603z' fill='%23000'/%3E%3C/svg%3E%0A")`,
+        WebkitMaskImage: `url("data:image/svg+xml,%3Csvg width='650' height='590' fill='none' viewBox='0 0 603 590' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m0 0v393h228v9.5c0 103.55 83.947 187.5 187.5 187.5s187.5-83.947 187.5-187.5v-402.5h-603z' fill='%23000'/%3E%3C/svg%3E%0A")`,
       }}
     >
       <div
         className={clsx(
-          'from-accent-400/20 via-accent-400/0 absolute top-0 right-0 h-[590px] w-[375px] rounded-full bg-gradient-to-t',
+          'from-accent-400/20 via-accent-400/0 absolute top-0 right-0 h-[610px] w-[375px] rounded-full bg-gradient-to-t',
           'dark:from-accent-600/10 dark:via-accent-600/0'
         )}
       >
         <div className={clsx('absolute right-0 bottom-0 overflow-hidden')}>
-          <m.div
+          {/* <m.div
             className={clsx('absolute z-[10]')}
             initial={{ opacity: 1 }}
             animate={controlsHeaderOutline}
@@ -46,11 +46,11 @@ function HeaderImage() {
                 });
               }}
             />
-          </m.div>
-          <m.div
+          </m.div> */}
+          <div
             className={clsx('')}
-            initial={{ opacity: 0 }}
-            animate={controlsHeaderImage}
+            // initial={{ opacity: 0 }}
+            // animate={controlsHeaderImage}
           >
             <Image
               alt="Benji Illustration"
@@ -65,7 +65,7 @@ function HeaderImage() {
               quality={100}
               priority
             />
-          </m.div>
+          </div>
         </div>
       </div>
     </div>
